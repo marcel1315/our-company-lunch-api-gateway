5a1699f97fa4a1511aa2ec93febcf778dffb8bb3
Marcel Lee <chungchung1315@gmail.com>
2024-10-26 22:54:17 +0900

    chore: Init project

---
+++ src/main/java/com/marceldev/ourcompanylunchapigateway/OurCompanyLunchApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.marceldev.ourcompanylunchapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurCompanyLunchApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OurCompanyLunchApiGatewayApplication.class, args);
+  }
+
+}
